dd4e3f4108e8a5763d290edcf52bf695038fc4df
Clayton <favero138@gmail.com>
2025-07-23 20:58:25 -0300

    Atualiza randon.py com novas alterações

--- randon.py
+++ randon.py
@@ -1,1 +1,8 @@
-print("Toma milkshake de morango!? Hmmmmmmmmm")
+#exercicio 1
+
+#print(len(input("Digite seu nome: ")))
+
+
+#exercicio 2 soma de valores
+print ("ano que você nasceu + sua idade")
+print(int(input("Sua idade: ")) + int(input("ano de nascimento: ")))
