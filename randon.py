d5617c6502c6d2f56101814c9bbeb860baebdc9f
Clayton <favero138@gmail.com>
2025-07-25 18:31:34 -0300

    quantidade de calorias semanais

--- randon.py
+++ randon.py
@@ -8,8 +8,19 @@
 #print(int(input("Sua idade: ")) + int(input("ano de nascimento: ")))
 
 #multiplicar calorias queimadas e dias de exercicio na semana
-print("calorias queimadas na semana")
+#print("calorias queimadas na semana")
+#dias_de_treino = int(input("quantos dias você treina na semana?"))
+#total_horas = float(input("quantas horas?"))
+#total_horas = dias_de_treino * total_horas
+#print(f"você treina {total_horas} horas por semana")
+
+#calorias
+print("calorias gastas na semana total")
 dias_de_treino = int(input("quantos dias você treina na semana?"))
-total_horas = float(input("quantas horas?"))
-total_horas = dias_de_treino * total_horas
-print(f"você treina {total_horas} horas por semana")
+horas_exercicio = float(input("quantos minutos por dia?"))
+total_horas = dias_de_treino * horas_exercicio
+caloria_por_hora = 8
+caloria_basal = 12000
+calorias_treino = total_horas * caloria_por_hora
+caloria_total_semana = calorias_treino + caloria_basal  
+print(f"gasta {caloria_total_semana} calorias por semana")
