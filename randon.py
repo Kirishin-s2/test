22489b585baa1914d4fc2d52babe2b4176c57cc0
Clayton <favero138@gmail.com>
2025-07-25 18:20:35 -0300

    multiplicação

--- randon.py
+++ randon.py
@@ -4,5 +4,12 @@
 
 
 #exercicio 2 soma de valores
-print ("ano que você nasceu + sua idade")
-print(int(input("Sua idade: ")) + int(input("ano de nascimento: ")))
+#print ("ano que você nasceu + sua idade")
+#print(int(input("Sua idade: ")) + int(input("ano de nascimento: ")))
+
+#multiplicar calorias queimadas e dias de exercicio na semana
+print("calorias queimadas na semana")
+dias_de_treino = int(input("quantos dias você treina na semana?"))
+total_horas = float(input("quantas horas?"))
+total_horas = dias_de_treino * total_horas
+print(f"você treina {total_horas} horas por semana")
